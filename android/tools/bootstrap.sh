dee4f5cbd389e5832560bf2cf355f502a7e019e5
Matjaž Drolc <mdrolc@gmail.com>
2016-07-15 11:25:44 +0200

    Updated build tools

--- android/tools/bootstrap.sh
+++ android/tools/bootstrap.sh
@@ -8,7 +8,7 @@ apt-get install -y --force-yes expect git wget libc6-i386 lib32stdc++6 lib32gcc1
 cd /opt && wget --output-document=android-sdk.tgz --quiet http://dl.google.com/android/android-sdk_r24.3.3-linux.tgz && tar xzf android-sdk.tgz && rm -f android-sdk.tgz && chown -R root.root android-sdk-linux
 
 chmod +x /opt/tools/android-accept-licenses.sh 
-/opt/tools/android-accept-licenses.sh "android update sdk --all --force --no-ui --filter platform-tools,tools,build-tools-23.0.2,android-23,extra-android-support,extra-android-m2repository,extra-google-m2repository,extra-google-google_play_services"
+/opt/tools/android-accept-licenses.sh "android update sdk --all --force --no-ui --filter platform-tools,tools,build-tools-23.0.3,android-23,extra-android-support,extra-android-m2repository,extra-google-m2repository,extra-google-google_play_services"
 
 #Cleanup
 apt-get autoremove
