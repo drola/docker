43621b28f523090916396b0d5bbf2b3d5a131531
Matjaž Drolc <mdrolc@gmail.com>
2017-01-03 21:37:12 +0100

    Added slimerjs + xvfb

--- node/bootstrap.sh
+++ node/bootstrap.sh
@@ -8,10 +8,24 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y build-essential nodejs nginx yarn
+apt-get install -y vim git wget xvfb libxrender-dev libasound2 libdbus-glib-1-2 libgtk2.0-0 bzip2 python
+
 apt-get autoclean
 apt-get clean
 
 npm install -g phantomjs-prebuilt
+
+
+
+export SLIMERJS_VERSION_F=0.9.6
+mkdir -p /srv/var
+wget -O /tmp/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2 http://download.slimerjs.org/releases/$SLIMERJS_VERSION_F/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2
+tar -xjf /tmp/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2 -C /tmp
+rm -f /tmp/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2
+mv /tmp/slimerjs-$SLIMERJS_VERSION_F/ /srv/var/slimerjs
+echo '#!/bin/bash\nxvfb-run /srv/var/slimerjs/slimerjs "$@"' > /srv/var/slimerjs/slimerjs.sh
+chmod 755 /srv/var/slimerjs/slimerjs.sh
+ln -s /srv/var/slimerjs/slimerjs.sh /usr/bin/slimerjs
 
 mkdir -p /var/www
 chown -R www-data:www-data /var/www
