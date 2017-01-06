d188d0aa69fd8d845de005ab66ca0e76b3ff178a
Matjaž Drolc <mdrolc@gmail.com>
2017-01-06 13:33:18 +0100

    Updated slimerjs version

--- node/bootstrap.sh
+++ node/bootstrap.sh
@@ -8,7 +8,7 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y build-essential nodejs nginx yarn
-apt-get install -y vim git wget xvfb libxrender-dev libasound2 libdbus-glib-1-2 libgtk2.0-0 bzip2 python supervisor cron
+apt-get install -y vim git wget xvfb libxrender-dev libasound2 libdbus-glib-1-2 libgtk2.0-0 bzip2 python supervisor cron firefox unzip
 
 apt-get autoclean
 apt-get clean
@@ -19,10 +19,10 @@ npm install -g phantomjs-prebuilt
 
 export SLIMERJS_VERSION_F=0.9.6
 mkdir -p /srv/var
-wget -O /tmp/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2 http://download.slimerjs.org/releases/$SLIMERJS_VERSION_F/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2
-tar -xjf /tmp/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2 -C /tmp
-rm -f /tmp/slimerjs-$SLIMERJS_VERSION_F-linux-x86_64.tar.bz2
-mv /tmp/slimerjs-$SLIMERJS_VERSION_F/ /srv/var/slimerjs
+wget -O /tmp/slimerjs.zip http://download.slimerjs.org/releases/0.10.2/slimerjs-0.10.2.zip
+cd /tmp && unzip /tmp/slimerjs.zip
+rm -f /tmp/slimerjs.zip
+mv /tmp/slimerjs-0.10.2/ /srv/var/slimerjs
 echo '#!/bin/bash\nxvfb-run /srv/var/slimerjs/slimerjs "$@"' > /srv/var/slimerjs/slimerjs.sh
 chmod 755 /srv/var/slimerjs/slimerjs.sh
 ln -s /srv/var/slimerjs/slimerjs.sh /usr/bin/slimerjs
